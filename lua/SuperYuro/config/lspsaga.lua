617193cc1bb28290ebe38f7ce096748151658b45
SuperYuro <a.gpgtdmgp@gmail.com>
2023-05-10 15:01:02 +0900

    Add default config per lspsaga commands

--- lua/SuperYuro/config/lspsaga.lua
+++ lua/SuperYuro/config/lspsaga.lua
@@ -14,25 +14,53 @@ lspsaga.setup {
   },
   request_timeout = 5000,
 
-  -- Async LSP Finder
-  finder = {
-    max_height = 0.5,
-    min_width = 30,
-    force_max_height = false,
-    keys = {
-      -- jump_to = 'p',
-      -- expand_or_jump = 'o',
-      vsplit = 'v',
-      split = 's',
-      tabnew = 't',
-      quit = { 'q', '<ESC>' },
-      close_in_preview = '<ESC>',
-    },
+  -- UI customization
+  ui = {
+    -- This option only works in Neovim 0.9
+    title = true,
+    -- Border type can be single, double, rounded, solid, shadow.
+    border = 'single',
+    winblend = 0,
+    expand = '',
+    collapse = '',
+    code_action = '💡',
+    incoming = ' ',
+    outgoing = ' ',
+    hover = ' ',
+    kind = {},
   },
 
-  -- Code Action
+  -- definition
+  definition = {
+    edit = "<C-c>o",
+    vsplit = "<C-c>v",
+    split = "<C-c>i",
+    tabe = "<C-c>t",
+    quit = "q",
+  }
+
+  -- hover
+  hover = {
+    max_width = 0.6,
+    open_link = 'gx',
+    open_browser = '!chrome',
+  },
+
+
+  -- type_definition
+
+  -- rename
+  rename = {
+    quit = '<C-c>',
+    exec = '<CR>',
+    mark = 'x',
+    confirm = '<CR>',
+    in_select = true,
+  },
+
+  -- code_action
   code_action = {
-    num_shortcut = true, -- Select code_action by number
+    num_shortcut = true,
     show_server_name = false,
     extend_gitsigns = true,
     keys = {
@@ -41,14 +69,56 @@ lspsaga.setup {
       exec = '<CR>',
     },
   },
+  lightbulb = {
+    enable = true,
+    enable_in_insert = true,
+    sign = true,
+    sign_priority = 40,
+    virtual_text = true,
+  },
+
+  -- references
+  finder = {
+    max_height = 0.5,
+    min_width = 30,
+    force_max_height = false,
+    keys = {
+      jump_to = 'p',
+      expand_or_jump = 'o',
+      vsplit = 's',
+      split = 'i',
+      tabe = 't',
+      tabnew = 'r',
+      quit = { 'q', '<ESC>' },
+      close_in_preview = '<ESC>',
+    },
+  },
+
+  -- outline
+  outline = {
+    win_position = 'right',
+    win_with = '',
+    win_width = 30,
+    preview_width = 0.4,
+    show_detail = true,
+    auto_preview = true,
+    auto_refresh = true,
+    auto_close = true,
+    custom_sort = nil,
+    keys = {
+      expand_or_jump = 'o',
+      quit = 'q',
+    },
+  },
 }
 
 -- Following settings are based on nvim-lspconfig's default settings.
-vim.keymap.set('n', 'gd', ':Lspsaga goto_definition<CR>', opts)             -- definition
-vim.keymap.set('n', 'K', ':Lspsaga hover_doc<CR>', opts)                    -- hover
-vim.keymap.set('n', '<C-k>', ':Lspsaga signature_help<CR>', opts)           -- signature_help
-vim.keymap.set('n', '<leader>D', ':Lspsaga goto_type_definition<CR>', opts) -- type_definition
-vim.keymap.set('n', '<leader>rn', ':Lspsaga rename<CR>', opts)              -- rename
-vim.keymap.set('n', '<leader>ca', ':Lspsaga code_action<CR>', opts)         -- code_action
-vim.keymap.set('v', '<leader>ca', ':Lspsaga range_code_action<CR>', opts)   -- code_action for selected buffer
-vim.keymap.set('n', 'gr', ':Lspsaga lsp_finder<CR>', opts)                  -- references
+vim.keymap.set('n', '<leader>e', ':Lspsaga show_line_diagnostics<CR>', opts) -- diagnostics
+vim.keymap.set('n', 'gd', ':Lspsaga goto_definition<CR>', opts)              -- definition
+vim.keymap.set('n', 'K', ':Lspsaga hover_doc<CR>', opts)                     -- hover
+vim.keymap.set('n', '<leader>D', ':Lspsaga goto_type_definition<CR>', opts)  -- type_definition
+vim.keymap.set('n', '<leader>rn', ':Lspsaga rename<CR>', opts)               -- rename
+vim.keymap.set('n', '<leader>ca', ':Lspsaga code_action<CR>', opts)          -- code_action
+vim.keymap.set('v', '<leader>ca', ':Lspsaga range_code_action<CR>', opts)    -- code_action for selected buffer
+vim.keymap.set('n', 'gr', ':Lspsaga lsp_finder<CR>', opts)                   -- references
+vim.keymap.set("n", "<C-o>", ":Lspsaga outline<CR>", opts)                   --outline
--- lua/SuperYuro/config/nvim-lspconfig.lua
+++ lua/SuperYuro/config/nvim-lspconfig.lua
@@ -37,7 +37,7 @@ mason_lsp.setup_handlers {
 }
 
 -- Global mappings.
-vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+-- vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
 vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
