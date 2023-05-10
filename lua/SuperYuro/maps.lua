c3d93442950616a985a0c57be76566ee7310145c
SuperYuro <a.gpgtdmgp@gmail.com>
2023-05-10 16:16:26 +0900

    Configure some global keymap

--- lua/SuperYuro/maps.lua
+++ lua/SuperYuro/maps.lua
@@ -1,4 +1,19 @@
+local opts = { silent = true, noremap = true }
+
 vim.g.mapleader = ' '
+
+-- Split buffer
+vim.keymap.set('n', 'ss', ':split<CR>', opts)
+vim.keymap.set('n', 'sv', ':vsplit<CR>', opts)
+
+-- Move windows
+vim.keymap.set('n', 'sh', '<C-w>h', opts)
+vim.keymap.set('n', 'sj', '<C-w>j', opts)
+vim.keymap.set('n', 'sk', '<C-w>k', opts)
+vim.keymap.set('n', 'sl', '<C-w>l', opts)
+
+-- Redo with shift+u
+vim.keymap.set('n', 'U', '<C-r>', opts)
 
 -- Don'y yank with x
 vim.keymap.set('n', 'x', '"_x')
