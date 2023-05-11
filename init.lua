4e1527943c9447dadb0b32c8fc297d6e3d909853
SuperYuro <a.gpgtdmgp@gmail.com>
2023-05-12 01:24:13 +0900

    Add shell settings per OS

--- init.lua
+++ init.lua
@@ -2,19 +2,26 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 require 'SuperYuro.base'
-require 'SuperYuro.maps'
 require 'SuperYuro.plugins'
+require 'SuperYuro.maps'
 
 local has = function(x)
   return vim.fn.has(x) == 1
 end
 
-local is_mac = has 'macunix'
+local is_mac = has 'mac'
 local is_win = has 'win32'
+local is_linux = has 'linux'
 
 if is_mac then
   vim.opt.clipboard:append { 'unnamedplus' }
 end
+
+if is_linux then
+  vim.opt.shell = 'fish'
+end
+
 if is_win then
+  vim.opt.shell = 'pwsh'
   vim.opt.clipboard:prepend { 'unnamed', 'unnamedplus' }
 end
--- lua/SuperYuro/base.lua
+++ lua/SuperYuro/base.lua
@@ -17,7 +17,6 @@ vim.opt.cmdheight = 1
 vim.opt.laststatus = 2
 vim.opt.expandtab = true
 vim.opt.scrolloff = 10
--- vim.opt.shell = "fish"
 vim.opt.backupskip = '/etc/*,/usr/bin/*,/usr/share/*,/usr/src/*'
 vim.opt.inccommand = 'split'
 vim.opt.ignorecase = true
