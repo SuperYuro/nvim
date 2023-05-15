5abb742698d4f8768a1c265f5589a203e518a54e
SuperYuro <a.gpgtdmgp@gmail.com>
2023-05-16 00:39:40 +0900

    Delete telescope-file-browser, configure to use instead of nvim-tree

--- lua/SuperYuro/config/nvim-tree.lua
+++ lua/SuperYuro/config/nvim-tree.lua
@@ -1,27 +1,14 @@
 require('nvim-tree').setup {
   sort_by = 'case_sensitive',
+  view = {
+    width = 30,
+  },
+  renderer = {
+    group_empty = true,
+  },
   filters = {
     dotfiles = true,
   },
-
-  -- Focus buffer when file opened
-  -- on_attach = function(bufnr)
-  --   local function opts(desc)
-  --     return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
-  --   end
-  --   local ok, api = pcall(require, 'nvim-tree.api')
-  --   assert(ok, 'api module is not found')
-  --   vim.keymap.set('n', '<CR>', api.node.open.tab_drop, opts 'Tab drop')
-  -- end,
 }
-vim.keymap.set('n', '<C-f>', ':NvimTreeToggle<CR>', { silent = true })
 
--- Autoclose
-vim.api.nvim_create_autocmd('BufEnter', {
-  nested = true,
-  callback = function()
-    if #vim.api.nvim_list_wins() == 1 and require('nvim-tree.utils').is_nvim_tree_buf() then
-      vim.cmd 'quit'
-    end
-  end,
-})
+vim.keymap.set('n', '<C-f>', ':NvimTreeToggle<CR>', { silent = true })
--- lua/SuperYuro/config/telescope.lua
+++ lua/SuperYuro/config/telescope.lua
@@ -4,7 +4,8 @@ local telescope = require 'telescope'
 telescope.setup {
   defaults = {
     mappings = {
-      i = {
+      n = {
+        ['q'] = actions.close,
         ['<ESC>'] = actions.close,
       },
     },
@@ -26,15 +27,6 @@ telescope.setup {
         vim.api.nvim_put({ emoji.value }, 'c', false, true)
       end,
     },
-    file_browser = {
-      hijack_netrw = true,
-      mappings = {
-        ['i'] = {
-          ['<ESC>'] = actions.close,
-          ['<C-o>'] = require('telescope').extensions.file_browser.actions.create,
-        },
-      },
-    },
   },
 }
 
@@ -42,7 +34,6 @@ telescope.load_extension 'software-licenses'
 telescope.load_extension 'emoji'
 telescope.load_extension 'ctags_outline'
 telescope.load_extension 'env'
-telescope.load_extension 'file_browser'
 
 local opts = { noremap = true, silent = true }
 vim.keymap.set('n', 'sr', ':Telescope live_grep<CR>', opts)
@@ -52,7 +43,6 @@ vim.keymap.set('n', 'sb', ':Telescope buffers<CR>', opts)
 vim.keymap.set('n', 'sd', ':Telescope diagnostics<CR>', opts)
 vim.keymap.set('n', 'se', ':Telescope env<CR>', opts)
 vim.keymap.set('n', 'st', ':Telescope ctags_outline outline<CR>', opts)
-vim.keymap.set('n', 'sf', ':Telescope file_browser path=%:p:h select_buffer=true hidden=true<CR>', opts)
 vim.keymap.set('n', 'sw', ':Telescope software-licenses find<CR>', opts)
 
 -- '<cmd>lua require("telescope").extensions.file_browser.file_browser({ path = "%:p:h", cws = telescope_buffer_dir(), respect_git_ignore = false, hidden = true, grouped = true, previewer = false, initial_mode = "normal", layout_config = { height = 40 } })<cr>',
