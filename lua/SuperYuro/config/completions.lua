3ae646b03f31c7a55a303b1ad5b4c52534ae5102
SuperYuro <a.gpgtdmgp@gmail.com>
2023-05-29 23:48:12 +0900

    Delete borderline in completion list

--- lua/SuperYuro/config/completions.lua
+++ lua/SuperYuro/config/completions.lua
@@ -1,64 +1,64 @@
-local cmp = require 'cmp'
+local cmp = require("cmp")
 
-cmp.setup {
-  view = 'wildmenu',
+cmp.setup({
+  view = "wildmenu",
 
   formatting = {
-    format = require('lspkind').cmp_format {
-      mode = 'symbol_text',
-      preset = 'codicons',
+    format = require("lspkind").cmp_format({
+      mode = "symbol_text",
+      preset = "codicons",
       maxwidth = 50,
-      ellipsis_char = '...',
-    },
+      ellipsis_char = "...",
+    }),
   },
   snippet = {
     expand = function(args)
-      vim.fn['vsnip#anonymous'](args.body)
+      vim.fn["vsnip#anonymous"](args.body)
     end,
   },
   window = {
-    completion = cmp.config.window.bordered(),
-    documentation = cmp.config.window.bordered(),
+    -- completion = cmp.config.window.bordered(),
+    -- documentation = cmp.config.window.bordered(),
   },
-  mapping = cmp.mapping.preset.insert {
-    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-g>'] = cmp.mapping.abort(),
-    ['<TAB>'] = cmp.mapping.confirm { select = true }, -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-  },
-  sources = cmp.config.sources({
-    { name = 'nvim_lsp' },
-    { name = 'nvim_lsp_signature_help' },
-    { name = 'vsnip' },
-  }, {
-    { name = 'buffer' },
+  mapping = cmp.mapping.preset.insert({
+    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-g>"] = cmp.mapping.abort(),
+    ["<TAB>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
   }),
-}
+  sources = cmp.config.sources({
+    { name = "nvim_lsp" },
+    { name = "nvim_lsp_signature_help" },
+    { name = "vsnip" },
+  }, {
+    { name = "buffer" },
+  }),
+})
 
 -- Set configuration for specific filetype.
-cmp.setup.filetype('gitcommit', {
+cmp.setup.filetype("gitcommit", {
   sources = cmp.config.sources({
-    { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
+    { name = "cmp_git" }, -- You can specify the `cmp_git` source if you were installed it.
   }, {
-    { name = 'buffer' },
+    { name = "buffer" },
   }),
 })
 
 -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline({ '/', '?' }, {
+cmp.setup.cmdline({ "/", "?" }, {
   mapping = cmp.mapping.preset.cmdline(),
   sources = {
-    { name = 'buffer' },
+    { name = "buffer" },
   },
 })
 
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline(':', {
+cmp.setup.cmdline(":", {
   mapping = cmp.mapping.preset.cmdline(),
   sources = cmp.config.sources({
-    { name = 'path' },
+    { name = "path" },
   }, {
-    { name = 'cmdline' },
+    { name = "cmdline" },
   }),
 })
