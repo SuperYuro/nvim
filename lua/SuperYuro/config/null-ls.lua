228920a6a22516b01554d44accef753f966affd1
SuperYuro <a.gpgtdmgp@gmail.com>
2023-06-29 16:32:12 +0900

    fix: change formatter for typescript

--- lua/SuperYuro/config/null-ls.lua
+++ lua/SuperYuro/config/null-ls.lua
@@ -58,7 +58,7 @@ null_ls.setup({
     null_ls.builtins.formatting.jq,
     -- Prettier
     null_ls.builtins.code_actions.eslint,
-    null_ls.builtins.formatting.prettierd,
+    null_ls.builtins.formatting.prettier,
   },
 })
 
