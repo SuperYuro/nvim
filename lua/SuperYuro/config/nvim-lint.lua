25073f72fd7a0a6a33f8c757217857b98a4de6b6
SuperYuro <a.gpgtdmgp@gmail.com>
2023-07-11 19:34:13 +0900

    feat: use nvim-lint instead of null-ls

--- lua/SuperYuro/config/nvim-lint.lua
+++ lua/SuperYuro/config/nvim-lint.lua
@@ -1,5 +1,23 @@
+local lint = require("lint")
+
 vim.api.nvim_create_autocmd({ "BufWritePost" }, {
   callback = function()
-    require("lint").try_lint()
+    lint.try_lint()
   end,
 })
+
+lint.linters_by_ft = {
+  c = { "cpplint" },
+  cpp = { "cpplint" },
+  python = { "flake8" },
+  javascript = { "eslint_d" },
+  javascriptreact = { "eslint_d" },
+  typescript = { "eslint_d" },
+  typescriptreact = { "eslint_d" },
+  astro = { "eslint_d" },
+  svelte = { "eslint_d" },
+  json = { "jsonlint" },
+  markdown = { "markdownlint" },
+  yaml = { "yamllint" },
+  fish = { "fish" },
+}
