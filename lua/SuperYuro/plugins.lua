455e6e76cf744807ddfb3b675ca8ab93dd292f9d
SuperYuro <a.gpgtdmgp@gmail.com>
2023-07-13 10:20:05 +0900

    feat: set key to launch lazygit

--- lua/SuperYuro/plugins.lua
+++ lua/SuperYuro/plugins.lua
@@ -385,6 +385,9 @@ require("lazy").setup({
   {
     "kdheepak/lazygit.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      vim.keymap.set("n", "<leader>gg", ":LazyGit<CR>", { noremap = true, silent = true })
+    end,
   },
 
   -- Git blame in buffer
