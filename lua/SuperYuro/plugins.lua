5467027089c789f4bf2a5d9f11712e86ea924429
SuperYuro <a.gpgtdmgp@gmail.com>
2023-07-13 10:17:23 +0900

    feat: change colorscheme

--- lua/SuperYuro/plugins.lua
+++ lua/SuperYuro/plugins.lua
@@ -18,19 +18,11 @@ require("lazy").setup({
 
   -- Colorscheme
   {
-    "shaunsingh/nord.nvim",
+    "projekt0n/github-nvim-theme",
     lazy = false,
     priority = 1000,
     config = function()
-      vim.g.nord_contrast = true
-      vim.g.nord_borders = true
-      vim.g.nord_disable_background = false
-      vim.g.nord_italic = true
-      vim.g.nord_uniform_diff_background = true
-      vim.g.nord_bold = false
-
-      require("nord").set()
-      -- vim.cmd("colorscheme nord")
+      vim.cmd("colorscheme github_light")
     end,
   },
 
