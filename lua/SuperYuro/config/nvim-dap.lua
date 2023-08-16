3793708f4e5fef6c6ba7baf4ed1989d10e391152
SuperYuro <a.gpgtdmgp@gmail.com>
2023-08-17 00:55:33 +0900

    chore: format code

--- lua/SuperYuro/config/nvim-dap.lua
+++ lua/SuperYuro/config/nvim-dap.lua
@@ -1,119 +1,119 @@
-local dap = require 'dap'
-local mason_dap = require 'mason-nvim-dap'
+local dap = require("dap")
+local mason_dap = require("mason-nvim-dap")
 
-mason_dap.setup {
+mason_dap.setup({
   ensure_installed = {
-    'js-debug-adapter',
-    'debugpy',
-    'bash-debug-adapter',
-    'codelldb',
+    "js-debug-adapter",
+    "debugpy",
+    "bash-debug-adapter",
+    "codelldb",
   },
-}
+})
 
 -- C/C++/Rust by codelldb
 dap.adapters.codelldb = {
-  type = 'server',
-  host = 'localhost',
-  port = '${port}',
+  type = "server",
+  host = "localhost",
+  port = "${port}",
   executable = {
-    command = vim.fn.stdpath 'data' .. '/mason/packages/codelldb/codelldb',
-    args = { '--port', '${port}' },
+    command = vim.fn.stdpath("data") .. "/mason/packages/codelldb/codelldb",
+    args = { "--port", "${port}" },
 
     -- Windows specified config
-    detached = not vim.fn.has 'win32',
+    detached = not vim.fn.has("win32"),
   },
 }
 -- JavaScript by js-debug-adapter
-dap.adapters['pwa-node'] = {
-  type = 'server',
-  host = 'localhost',
-  port = '${port}',
+dap.adapters["pwa-node"] = {
+  type = "server",
+  host = "localhost",
+  port = "${port}",
   executable = {
-    command = 'node',
-    args = { vim.fn.stdpath 'data' .. '/mason/packages/js-debug-adapter/js-debug-adapter', '${port}' },
+    command = "node",
+    args = { vim.fn.stdpath("data") .. "/mason/packages/js-debug-adapter/js-debug-adapter", "${port}" },
   },
 }
 dap.configurations.javascript = {
-  type = 'pwa-node',
-  request = 'launch',
-  name = 'Launch file',
-  program = '${file}',
-  cwd = '${workspaceFolder}',
+  type = "pwa-node",
+  request = "launch",
+  name = "Launch file",
+  program = "${file}",
+  cwd = "${workspaceFolder}",
 }
 -- Python by debugpy
 dap.adapters.python = function(cb, config)
-  if config.request == 'attach' then
+  if config.request == "attach" then
     ---@diagnostic disable-next-line: undefined-field
     local port = (config.connect or config).port
     ---@diagnostic disable-next-line: undefined-field
-    local host = (config.connect or config).host or '127.0.0.1'
-    cb {
-      type = 'server',
-      port = assert(port, '`connect.port` is required for a python `attach` configuration'),
+    local host = (config.connect or config).host or "127.0.0.1"
+    cb({
+      type = "server",
+      port = assert(port, "`connect.port` is required for a python `attach` configuration"),
       host = host,
       options = {
-        source_filetype = 'python',
+        source_filetype = "python",
       },
-    }
+    })
   else
-    cb {
-      type = 'executable',
-      command = vim.fn.stdpath 'data' .. '/mason/packages/debugpy/debugpy-adapter',
+    cb({
+      type = "executable",
+      command = vim.fn.stdpath("data") .. "/mason/packages/debugpy/debugpy-adapter",
       options = {
-        source_filetype = 'python',
+        source_filetype = "python",
       },
-    }
+    })
   end
 end
 dap.configurations.python = {
   {
     -- The first three options are required by nvim-dap
-    type = 'python', -- the type here established the link to the adapter definition: `dap.adapters.python`
-    request = 'launch',
-    name = 'Launch file',
+    type = "python", -- the type here established the link to the adapter definition: `dap.adapters.python`
+    request = "launch",
+    name = "Launch file",
 
     -- Options below are for debugpy, see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for supported options
 
-    program = '${file}', -- This configuration will launch the current file if used.
+    program = "${file}", -- This configuration will launch the current file if used.
     pythonPath = function()
       -- debugpy supports launching an application with a different interpreter then the one used to launch debugpy itself.
       -- The code below looks for a `venv` or `.venv` folder in the current directly and uses the python within.
       -- You could adapt this - to for example use the `VIRTUAL_ENV` environment variable.
       local cwd = vim.fn.getcwd()
-      if vim.fn.executable(cwd .. '/venv/bin/python') == 1 then
-        return cwd .. '/venv/bin/python'
-      elseif vim.fn.executable(cwd .. '/.venv/bin/python') == 1 then
-        return cwd .. '/.venv/bin/python'
+      if vim.fn.executable(cwd .. "/venv/bin/python") == 1 then
+        return cwd .. "/venv/bin/python"
+      elseif vim.fn.executable(cwd .. "/.venv/bin/python") == 1 then
+        return cwd .. "/.venv/bin/python"
       else
-        return '/usr/bin/python'
+        return "/usr/bin/python"
       end
     end,
   },
 }
 -- Bash
 dap.adapters.bashdb = {
-  type = 'executable',
-  command = vim.fn.stdpath 'data' .. '/mason/packages/bash-debug-adapter/bash-debug-adapter',
-  name = 'bashdb',
+  type = "executable",
+  command = vim.fn.stdpath("data") .. "/mason/packages/bash-debug-adapter/bash-debug-adapter",
+  name = "bashdb",
 }
 dap.configurations.sh = {
   {
-    type = 'bashdb',
-    request = 'launch',
-    name = 'Launch file',
+    type = "bashdb",
+    request = "launch",
+    name = "Launch file",
     showDebugOutput = true,
-    pathBashdb = vim.fn.stdpath 'data' .. '/mason/packages/bash-debug-adapter/extension/bashdb_dir/bashdb',
-    pathBashdbLib = vim.fn.stdpath 'data' .. '/mason/packages/bash-debug-adapter/extension/bashdb_dir',
+    pathBashdb = vim.fn.stdpath("data") .. "/mason/packages/bash-debug-adapter/extension/bashdb_dir/bashdb",
+    pathBashdbLib = vim.fn.stdpath("data") .. "/mason/packages/bash-debug-adapter/extension/bashdb_dir",
     trace = true,
-    file = '${file}',
-    program = '${file}',
-    cwd = '${workspaceFolder}',
-    pathCat = 'cat',
-    pathBash = '/bin/bash',
-    pathMkfifo = 'mkfifo',
-    pathPkill = 'pkill',
+    file = "${file}",
+    program = "${file}",
+    cwd = "${workspaceFolder}",
+    pathCat = "cat",
+    pathBash = "/bin/bash",
+    pathMkfifo = "mkfifo",
+    pathPkill = "pkill",
     args = {},
     env = {},
-    terminalKind = 'integrated',
+    terminalKind = "integrated",
   },
 }
