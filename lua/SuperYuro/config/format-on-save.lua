8b70a09f60a3a499309bdc0f3d437f3781a8b64b
SuperYuro <a.gpgtdmgp@gmail.com>
2023-09-07 12:30:00 +0900

    fix: delete to format with prettier

--- lua/SuperYuro/config/format-on-save.lua
+++ lua/SuperYuro/config/format-on-save.lua
@@ -1,14 +1,6 @@
 local format_on_save = require("format-on-save")
 local formatters = require("format-on-save.formatters")
 
-local lsp_or_prettierd = {
-  formatters.lsp,
-  formatters.if_file_exists({
-    pattern = { ".prettierrc", "prettierrc.*" },
-    formatter = formatters.prettierd,
-  }),
-}
-
 local clang_format = formatters.shell({
   cmd = { "clang-format", "--assume-filename", "%" },
 })
@@ -28,21 +20,18 @@ format_on_save.setup({
     lua = formatters.stylua,
     sh = formatters.shfmt,
     dart = formatters.lsp,
-    json = formatters.jq,
+    json = formatters.lsp,
     yaml = formatters.lsp,
-    html = lsp_or_prettierd,
-    css = lsp_or_prettierd,
-    javascript = lsp_or_prettierd,
-    typescript = lsp_or_prettierd,
-    typescriptreact = lsp_or_prettierd,
-    astro = lsp_or_prettierd,
-    svelte = lsp_or_prettierd,
-    python = {
-      formatters.black,
-      -- formatters.isort
-    },
+    html = formatters.lsp,
+    css = formatters.lsp,
+    javascript = formatters.lsp,
+    typescript = formatters.lsp,
+    typescriptreact = formatters.lsp,
+    astro = formatters.lsp,
+    svelte = formatters.lsp,
+    python = formatters.black,
   },
-  run_with_sh = vim.fn.has("win32") ~= 1,
+  -- run_with_sh = vim.fn.has("win32") ~= 1,
 })
 
 format_on_save.restore_cursors()
