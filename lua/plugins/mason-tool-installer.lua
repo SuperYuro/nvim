f88fe9e825164d5681026c945dd9ff7213d72a0f
SuperYuro <a.gpgtdmgp@gmail.com>
2023-11-19 03:15:04 +0900

    feat: add mason-tool-installer.nvim

---
+++ lua/plugins/mason-tool-installer.lua
@@ -0,0 +1,13 @@
+return {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    lazy = false,
+    opts = {
+        ensure_installed = {
+            -- Formatters
+            "stylua",
+            -- Linters
+        },
+        auto_update = true,
+        run_on_start= true,
+    }
+}
