4280a8f75dfb01bb6f7bd4868bcdc9a65c2f8438
SuperYuro <a.gpgtdmgp@gmail.com>
2023-11-19 02:00:44 +0900

    feat: configure nvim-lspconfig and mason

---
+++ lua/plugins/lspconfig.lua
@@ -0,0 +1,53 @@
+local ensure_installed = {
+    "lua_ls",
+}
+
+return {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+    },
+    lazy = false,
+    config = function()
+        require("mason").setup()
+
+        require("mason-lspconfig").setup({
+            ensure_installed = ensure_installed,
+            automatic_installation = false,
+        })
+
+        require("mason-lspconfig").setup_handlers {
+            function (server_name) -- default handler (optional)
+                local on_attach = nil
+                local capabilities = nil
+
+                require("lspconfig")[server_name].setup ({
+                    on_attach = on_attach,
+                    capabilities = capabilities,
+                })
+            end,
+           ["lua_ls"] = function ()
+               local lspconfig = require("lspconfig")
+               lspconfig.lua_ls.setup {
+                   settings = {
+                       Lua = {
+                           diagnostics = {
+                               globals = { "vim" }
+                           }
+                       }
+                   }
+               }
+           end,
+        }
+        -- Use LspAttach autocommand to only map the following keys
+        -- after the language server attaches to the current buffer
+        vim.api.nvim_create_autocmd('LspAttach', {
+            group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+            callback = function(ev)
+            -- Enable completion triggered by <c-x><c-o>
+            vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+        end,
+        })
+    end,
+}
