22ff7335e2a9fb92c3442b3fdc9bad3f3508c0f2
SuperYuro <a.gpgtdmgp@gmail.com>
2023-11-19 02:51:17 +0900

    feat: configure treesitter

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,24 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    lazy = false,
+    dependencies = {
+        {
+            "nvim-treesitter/nvim-treesitter-context",
+            opts = {
+                enable = true,
+            },
+        },
+        "HiPhish/nvim-ts-rainbow2",
+    },
+    config = function ()
+        require("nvim-treesitter.configs").setup({
+            ensure_installed = "all",
+
+            highlight = {
+                enable = true,
+            },
+            rainbow = { enable = true }, -- nvim-ts-rainbow2
+        })
+    end
+}
