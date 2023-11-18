a66b46721f7cc4bfec7ce1251778cf54feb4a443
SuperYuro <a.gpgtdmgp@gmail.com>
2023-11-18 14:05:16 +0900

    feat: change colorscheme

---
+++ lua/plugins/nightfox.lua
@@ -0,0 +1,17 @@
+local opts = {
+    options = {
+        inverse = {
+            visual = true,
+        },
+    },
+}
+
+return {
+    "EdenEast/nightfox.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+        require("nightfox").setup(opts)
+        vim.cmd("colorscheme nordfox")
+    end,
+}
