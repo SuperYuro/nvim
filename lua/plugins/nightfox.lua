baef99c1b7b194bf2d5a12e1cd1685506a10dc7f
SuperYuro <a.gpgtdmgp@gmail.com>
2023-11-20 23:53:22 +0900

    fix: nightfox config opts

--- lua/plugins/nightfox.lua
+++ lua/plugins/nightfox.lua
@@ -1,16 +1,15 @@
-local opts = {
-  options = {
-    inverse = {
-      visual = true,
-    },
-  },
-}
-
 return {
   "EdenEast/nightfox.nvim",
   lazy = false,
   priority = 1000,
-  config = function()
+  opts = {
+    options = {
+      inverse = {
+        visual = true,
+      },
+    },
+  },
+  config = function(_, opts)
     require("nightfox").setup(opts)
     vim.cmd("colorscheme nordfox")
   end,
