cc482e06035598800f5aa2c17b32b25bc4d20079
SuperYuro <a.gpgtdmgp@gmail.com>
2023-12-02 20:38:35 +0900

    chore: format code

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,9 +6,7 @@ return {
     require("nvim-treesitter.configs").setup({
       ensure_installed = "all",
 
-      highlight = {
-        enable = true,
-      },
+      highlight = { enable = true },
     })
   end,
 }
