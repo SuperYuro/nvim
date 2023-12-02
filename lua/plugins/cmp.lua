0c9d76a6176ecf5785bb0d788b9e0b2bfc40ac9d
SuperYuro <a.gpgtdmgp@gmail.com>
2023-12-02 20:38:29 +0900

    feat: show tailwindcss color for cmp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -14,6 +14,12 @@ return {
       event = "InsertEnter",
     },
     { "onsails/lspkind-nvim" },
+    {
+      "roobert/tailwindcss-colorizer-cmp.nvim",
+      opts = {
+        color_square_width = 2,
+      },
+    },
   },
   config = function()
     local cmp = require("cmp")
@@ -27,18 +33,12 @@ return {
         },
       },
       formatting = {
-        fields = { "kind", "abbr", "menu" },
-        format = function(entry, vim_item)
-          local kind = require("lspkind").cmp_format({ mode = "symbol_text", maxwidth = 50 })(entry, vim_item)
-          local strings = vim.split(kind.kind, "%s", { trimempty = true })
-          kind.kind = " " .. (strings[1] or "") .. " "
-          kind.menu = "    (" .. (strings[2] or "") .. ")"
-
-          return kind
-        end,
+        format = require("tailwindcss-colorizer-cmp").formatter,
       },
       snippet = {
-        expand = function(args) require("luasnip").lsp_expand(args.body) end,
+        expand = function(args)
+          require("luasnip").lsp_expand(args.body)
+        end,
       },
       mapping = cmp.mapping.preset.insert({
         ["<C-b>"] = cmp.mapping.scroll_docs(-4),
