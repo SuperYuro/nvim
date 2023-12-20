3a2913c0d73bf6315c207e2ea97cb2c20f406f88
SuperYuro <a.gpgtdmgp@gmail.com>
2023-12-20 15:31:27 +0900

    feat: configure vim-illuminate

---
+++ lua/plugins/illuminate.lua
@@ -0,0 +1,13 @@
+return {
+  "RRethy/vim-illuminate",
+  event = "BufRead",
+  opts = {
+    providers = {
+      "lsp",
+      "treesitter",
+    },
+  },
+  config = function(_, opts)
+    require("illuminate").configure(opts)
+  end,
+}
