c1267a072a59a1e7367ab71bf8b3aafcf30d9a55
SuperYuro <a.gpgtdmgp@gmail.com>
2023-12-26 23:36:51 +0900

    fix: hop keybind

--- lua/plugins/hop.lua
+++ lua/plugins/hop.lua
@@ -2,7 +2,7 @@ return {
   "smoka7/hop.nvim",
   version = "*",
   keys = {
-    { "f", "<cmd>HopWord<cr>", desc = "Hop word" },
+    { "<leader><leader>", "<cmd>HopWord<cr>", desc = "Hop word" },
   },
   opts = {},
 }
