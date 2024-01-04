fc14bb1c5e87746fa204ad7d5280752ed8b1efd2
SuperYuro <a.gpgtdmgp@gmail.com>
2024-01-05 00:57:18 +0900

    feat: noice extension for inc-rename

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -10,6 +10,7 @@ return {
         timeout = 1000,
       },
     },
+    "smjonas/inc-rename.nvim",
   },
   opts = {
     lsp = {
@@ -28,7 +29,7 @@ return {
       bottom_search = true, -- use a classic bottom cmdline for search
       command_palette = true, -- position the cmdline and popupmenu together
       long_message_to_split = true, -- long messages will be sent to a split
-      inc_rename = false, -- enables an input dialog for inc-rename.nvim
+      inc_rename = true, -- enables an input dialog for inc-rename.nvim
       lsp_doc_border = false, -- add a border to hover docs and signature help
     },
   },
