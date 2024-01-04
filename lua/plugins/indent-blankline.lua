fa409d015a51d1be6a912d28cdf24c0cc14384c0
SuperYuro <a.gpgtdmgp@gmail.com>
2024-01-05 00:55:50 +0900

    feat: disable indent-rainbow

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,9 +1,6 @@
 return {
   "lukas-reineke/indent-blankline.nvim",
   event = "VeryLazy",
-  dependencies = { "TheGLander/indent-rainbowline.nvim" },
   main = "ibl",
-  opts = function(_, opts)
-    return require("indent-rainbowline").make_opts(opts)
-  end,
+  opts = {},
 }
