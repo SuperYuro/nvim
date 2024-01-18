d4bbddad8ec5efa57b27a250f6f11f9cb6fa7307
SuperYuro <a.gpgtdmgp@gmail.com>
2024-01-18 22:51:37 +0900

    fix: merge config for treesitter and yati

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,11 +2,16 @@ return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
   lazy = false,
+  dependencies = { { "yioneko/nvim-yati", tag = "*" } },
   config = function()
     require("nvim-treesitter.configs").setup({
       ensure_installed = "all",
-
       highlight = { enable = true },
+      yati = {
+        enable = true,
+        default_lazy = true,
+      },
+      indent = { enable = false },
     })
   end,
 }
--- lua/plugins/yati.lua
+++ lua/plugins/yati.lua
@@ -1,17 +1,9 @@
 return {
   "yioneko/nvim-yati",
+  tag = "*",
   event = "VeryLazy",
   dependencies = { "nvim-treesitter/nvim-treesitter" },
-  opts = {},
   config = function(_, opts)
-    require("nvim-treesitter.configs").setup({
-      yati = {
-        enable = true,
-        default_lazy = true,
-      },
-      indent = {
-        enable = false,
-      },
-    })
+    require("nvim-treesitter.configs").setup({})
   end,
 }
