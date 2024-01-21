cfe6c0f211d67da774732ab184f08515d48059cd
SuperYuro <a.gpgtdmgp@gmail.com>
2024-01-21 21:01:52 +0900

    feat: configure modes.nvim

---
+++ lua/plugins/mode.lua
@@ -0,0 +1,12 @@
+return {
+  "mvllow/modes.nvim",
+  version = "v0.2.0",
+  event = "VeryLazy",
+  opts = {
+    ignore_filetypes = {
+      "NvimTree",
+      "TelescopePrompt",
+      "lazy",
+    },
+  },
+}
