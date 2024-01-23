986f054e00e6565c0d2b305f6396845d3673314e
SuperYuro <a.gpgtdmgp@gmail.com>
2024-01-23 20:48:01 +0900

    feat: change Python default linter to ruff

--- lua/plugins/nvim-lint.lua
+++ lua/plugins/nvim-lint.lua
@@ -3,7 +3,7 @@ return {
   event = "VeryLazy",
   config = function()
     require("lint").linters_by_ft = {
-      python = { "flake8" },
+      python = { "ruff" },
     }
 
     vim.api.nvim_create_autocmd({ "BufWritePost" }, {
