e53c4b1a82ecfb2092fdeba1afc401068960bee0
SuperYuro <a.gpgtdmgp@gmail.com>
2024-03-14 15:39:05 +0900

    fix: aerial lazy-load

--- lua/plugins/aerial.lua
+++ lua/plugins/aerial.lua
@@ -5,11 +5,10 @@ return {
     "nvim-tree/nvim-web-devicons",
     "nvim-telescope/telescope.nvim",
   },
-  lazy = false,
-  opts = {},
   keys = {
     { "<leader>ar", "<cmd>Telescope aerial<cr>", desc = "Telescope aerial" },
   },
+  opts = {},
   config = function(_, opts)
     require("aerial").setup(opts)
     require("telescope").load_extension("aerial")
